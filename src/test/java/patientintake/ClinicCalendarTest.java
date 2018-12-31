c0b90b5c03eab2bb22a241a1af3ddd073d334f0e
Jason Kilgrow <jkilgrow@doterra.com>
2018-12-31 13:29:34 -0700

    had to rename the test class to have Test in the name so that maven would detect and run the test

--- src/test/java/patientintake/ClinicCalendarTest.java
+++ src/test/java/patientintake/ClinicCalendarTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClinicCalendarShould
+class ClinicCalendarTest
 {
     @Test
     public void testAllowEntryOfAnAppointment()
